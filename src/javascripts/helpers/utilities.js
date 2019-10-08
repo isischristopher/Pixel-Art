99039c15f371efed5af4dad66fe5d55a6d54e3b0
Isis Christopher <isischristopher@gmail.com>
2019-10-07 19:27:45 -0500

    added grids

---
+++ src/javascripts/helpers/utilities.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+
+const printToDom = (divId, toPrint) => {
+  $(`#${divId}`).html(toPrint);
+
+
+  document.getElementById(divId).innerHTML = toPrint;
+};
+
+export default { printToDom };
